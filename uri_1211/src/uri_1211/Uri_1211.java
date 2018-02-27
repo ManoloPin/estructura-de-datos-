84fdfd6ecc5c89dc3c9da4c5800010bedb06a4d5
ManuelPinillla <jpinillaca1@uniminuto.edu.co>
2018-02-27 17:58:15 -0500

    Add files via upload

---
+++ uri_1211/src/uri_1211/Uri_1211.java
@@ -0,0 +1,50 @@
+package uri_1211;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Uri_1211 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            String input = br.readLine();
+            int cases;
+            int[][] numbers;
+            while (!input.equals("") && !input.equals(0)) {
+                
+                cases = Integer.parseInt(input);
+                input = br.readLine();
+                numbers = new int[cases][input.length()];
+
+                for (int j = 0; j < input.length(); j++) {
+                    numbers[0][j] = Integer.parseInt(input.substring(j, j + 1));
+                }
+
+                for (int i = 0; i < cases; i++) {
+                    input = br.readLine();
+                    for (int j = 0; j < input.length(); j++) {
+                        numbers[i][j] = Integer.parseInt(input.substring(j, j + 1));
+                    }
+                }
+                input = br.readLine();
+            }
+        } catch (Exception ex) {}
+    } 
+    
+    public static void redixSort(int [][]numbers){
+        for (int j = numbers[0].length-1;j>=0;j--){
+            
+        }
+    }
+}
